1d51ba16717b4114e50cac1880a1b3aad56f17ae
Karl J. Overå <k@rl.run>
2018-09-22 15:31:07 +0200

    Add screenshot pixel matching to puppeteer test

--- tweet-embed.spec.e2e.js
+++ tweet-embed.spec.e2e.js
@@ -1,9 +1,12 @@
 import test from 'ava'
 import puppeteer from 'puppeteer'
+import ScreenshotTester from 'puppeteer-screenshot-tester'
 
-let browser, page
+let browser, page, tester
 test.before('start e2e app', async () => {
-  console.log('Before')
+  tester = await ScreenshotTester({
+    threshold: 0.1
+  })
   browser = await puppeteer.launch({
     args: ['--no-sandbox'],
     headless: true
@@ -19,20 +22,15 @@ test.before('start e2e app', async () => {
   })
 })
 
-test('does the thing', async t => {
+test('screenshots are identical', async t => {
   await page.goto('http://localhost:3000/')
   await page.waitForSelector('twitterwidget')
 
-  await page.screenshot({ path: 'tweet-embed.spec.e2e.png' })
+  const identicalScreenshots = await tester(page, 'tweet-embed.spec.e2e')
 
-  // const html = await page.$('twitterwidget')
-
-  // TODO add pixelmatch
-
-  t.truthy(true)
+  t.true(identicalScreenshots)
 })
 
 test.after.always('close e2e app', async () => {
-  console.log('After')
   browser.close()
 })
